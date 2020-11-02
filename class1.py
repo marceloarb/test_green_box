e30c888282a8184ffbb6f665fd88872789b63b0d
Marcelo Barbosa <mbarbosa@teksystems.com>
2020-11-02 15:51:35 -0600

    created test2

--- class1.py
+++ class1.py
@@ -1,3 +1,10 @@
 class math():
     def square(arg1,arg2):
         return arg1*arg2
+    def iterate(arg1):
+        arr = []
+        for i in len(arg1):
+            arr.append(arg1[i])
+        return arr
+        
+
---
+++ test2.py
@@ -0,0 +1,4 @@
+from class1 import math
+
+def test_iterate():
+    assert math.iterate([3,4,5,6]) == [3,4,5,6], "The array should be the same"
