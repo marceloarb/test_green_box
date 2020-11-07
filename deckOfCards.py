80446432fe3184b4f34dade9ad10b984f1ad5549
Marcelo Barbosa <mbarbosa@teksystems.com>
2020-11-07 16:50:17 -0600

    Shuffle deck method and test implemented

--- deckOfCards.py
+++ deckOfCards.py
@@ -1,13 +1,21 @@
 from card import card
+from random import shuffle 
 
 class deckOfCards:
     def __init__(self):
+        self.deck = []
         self.suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
         self.values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
     def create_deck(self):
-        deck = []
         for suit in self.suits:
             for value in self.values:
-                deck.append(value +" "+ suit)
-        return deck
+                self.deck.append(value +" "+ suit)
+        return self.deck
+    def shuffle_deck(self):
+        self.create_deck()
+        if len(self.deck) < 52:
+            print("Cannot shuffle the cards")
+        else:
+            shuffle(self.deck)
+            return self.deck
     
--- tdd1.py
+++ tdd1.py
@@ -7,8 +7,7 @@ class TestDeckCards(unittest.TestCase):
         length = len(deckOfCards().create_deck())
         actual = length
         self.assertEqual(actual,expected)
-    def test_Deck_Of_Cards_Empty(self):
-        expected = 0
-        length = len(deckOfCards().create_deck())
-        actual = length
-        self.assertEqual(actual,expected)
+    def test_Shuffle_Deck_Of_Cards(self):
+        expected = deckOfCards().create_deck()
+        actual = deckOfCards().shuffle_deck()
+        self.assertNotEqual(actual,expected)
