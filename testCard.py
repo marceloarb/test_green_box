e3990b1d6f5eb933f5251c3b82d437dfd5cfea9f
Marcelo Barbosa <mbarbosa@teksystems.com>
2020-11-10 13:31:35 -0600

    test_card_had_is_random testing comparing two hands and they are different

--- testCard.py
+++ testCard.py
@@ -2,8 +2,12 @@ import unittest
 from card import card
 
 class TestCard(unittest.TestCase):
-    def test_card_hand(self):
+    def test_card_hand_length(self):
         expected = 2
         actual = card().user_hand_texas_holden()
         actual = len(actual)
         self.assertEqual(actual,expected)
+    def test_card_hand_is_random(self):
+        expected = card().user_hand_texas_holden()
+        actual = card().user_hand_texas_holden()
+        self.assertNotEqual(actual,expected)
