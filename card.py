b90094e3335911d6e7f51483e969c7526b2fc53b
Marcelo Barbosa <mbarbosa@teksystems.com>
2020-11-24 16:53:38 -0600

    update

--- card.py
+++ card.py
@@ -6,12 +6,7 @@ class card():
                 "2", "3", "4", "5", "6", "7", "8", "9", "10", 
                 "Jack", "Queen", "King", "Ace"
             )
-    def user_hand_texas_holden(self):
-        print(self.deck_of_cards)
-        hand = []
-        hand.append(self.deck_of_cards.pop(len(self.deck_of_cards)-1))
-        hand.append(self.deck_of_cards.pop(len(self.deck_of_cards)-1))
-        return hand
+    
 
 
     
--- player.py
+++ player.py
@@ -4,4 +4,8 @@ class Player():
         self.name = name
         self.hand = hand
     def add_cards(self, cards):
-        self.hand.add_cards(cards)
+        print(self.deck_of_cards)
+        hand = []
+        hand.append(self.deck_of_cards.pop(len(self.deck_of_cards)-1))
+        hand.append(self.deck_of_cards.pop(len(self.deck_of_cards)-1))
+        return hand
