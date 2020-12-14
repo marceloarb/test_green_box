249546d199513108f66bc021376797a63115a94b
Marcelo Barbosa <mbarbosa@teksystems.com>
2020-12-14 10:05:25 -0600

    Update

--- testDeckOfCards.py
+++ testDeckOfCards.py
@@ -10,6 +10,11 @@ class TestDeckCards(unittest.TestCase):
         self.assertEqual(actual,expected)
     #Ask Josh if you can check if the deck of cards empty can be checked like that or if we need to check the same function like the one above.
     
+    def test_Deck_Is_Shuffled(self):
+        expected = Deck().cards
+        actual = Deck().shuffle()
+        actual = Deck().cards
+        self.assertNotEqual(actual,expected)
 
 if __name__ == '__main__':
     unittest.main()
